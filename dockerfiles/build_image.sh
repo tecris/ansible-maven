676e9bbbb1c08a54dbdab996c879d64d59d0495f
Eugen Cristea <eugen.cristea@prolion.com>
2023-03-26 22:03:34 +1300

    molecule testing

--- dockerfiles/build_image.sh
+++ dockerfiles/build_image.sh
@@ -9,5 +9,14 @@ fi
 os=${1}
 version=${2}
 
-docker build -t org.tecris/${os}.${version}-jdk:23.03.26 -f Dockerfile.${os}_jdk.${version} ./
-#docker build --no-cache -t org.tecris/${os}.${version}-jdk:23.03.26 -f Dockerfile.${os}_jdk.${version} ./
+# https://github.com/adoptium/temurin17-binaries/releases/latest
+jdk=OpenJDK17U-jdk_x64_linux_hotspot_17.0.6_10.tar.gz
+
+if [ ! -f ${jdk} ]; then
+    echo "File ${jdk} not found, download"
+    wget https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.6%2B10/OpenJDK17U-jdk_x64_linux_hotspot_17.0.6_10.tar.gz
+fi
+
+docker build --no-cache \
+    -t org.tecris/${os}-${version}-jdk:23.03.26 \
+    -f Dockerfile.${os}_jdk.${version} ./
--- molecule/ubuntu_22.04/tests/test_default.py
+++
@@ -1,8 +0,0 @@
-"""Role testing files using testinfra."""
-
-
-def test_maven_path(host):
-    """Validate maven path."""
-    f = host.file("/etc/profile.d/maven.sh")
-
-    assert f.contains("M2_HOME=/opt/apache-maven-3.9.0")
