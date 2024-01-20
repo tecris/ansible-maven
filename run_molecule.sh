5ba6dfe6602c26719b9b262c796a10413075ab27
tecristea <tecristea@gmail.com>
2024-01-20 21:56:17 +1300

    molecule script IMAGE_TAG parameter

--- run_molecule.sh
+++ run_molecule.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 # set -x
+ 
+IMAGE_TAG=$(date +"%y.%m.%d")
 
 docker run --rm -it \
     -v $(pwd):/molecule/ansible-maven \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -w /molecule/ansible-maven \
-    org.tecris/molecule6:23.11.04 \
+    org.tecris/molecule6:${IMAGE_TAG} \
     "$@"
