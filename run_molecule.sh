8c0136db7d9500b800f9d30a1bd3306a11201b70
tecristea <tecristea@gmail.com>
2024-01-28 21:01:30 +1300

    lock down molecule image version

--- run_molecule.sh
+++ run_molecule.sh
@@ -8,5 +8,5 @@ docker run --rm -it \
     -v $(pwd):/molecule/ansible-maven \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -w /molecule/ansible-maven \
-    org.tecris/molecule6:${IMAGE_TAG} \
+    tecristea/molecule6:24.01.20 \
     "$@"
