92a7a68b775a933c39b6087d50559d4bcfe44697
tecristea <tecristea@gmail.com>
2024-09-14 22:54:23 +0300

    MAVEN:3.9.7 clean up

--- molecule/debian_10/tests/test_default.py
+++
@@ -1,9 +0,0 @@
-"""Role testing files using testinfra."""
-
-def test_distribution(host):
-    """Validate distribution release."""
-    f = host.file("/etc/os-release")
-
-    assert f.contains("ID=debian")
-    assert f.contains("VERSION_ID=\"10\"")
-
--- molecule/ubuntu_18.04/tests/test_default.py
+++
@@ -1,9 +0,0 @@
-"""Role testing files using testinfra."""
-
-
-def test_distribution(host):
-    """Validate distribution release."""
-    f = host.file("/etc/lsb-release")
-
-    assert f.contains("DISTRIB_ID=Ubuntu")
-    assert f.contains("DISTRIB_RELEASE=18.04")
