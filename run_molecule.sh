565f00d6e981d108e971dc5618e885cca96501bb
tecristea <tecristea@gmail.com>
2024-09-14 22:51:56 +0300

    MAVEN:3.9.7 tooling

--- run_molecule.sh
+++ run_molecule.sh
@@ -8,5 +8,5 @@ docker run --rm -it \
     -v $(pwd):/molecule/ansible-maven \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -w /molecule/ansible-maven \
-    tecristea/molecule6:24.01.20 \
+    org.tecris/molecule:24.09.14 \
     "$@"
